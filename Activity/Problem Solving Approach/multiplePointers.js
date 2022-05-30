4616203ff2ee50e2d24e090e8fb83040dd2d67f0
Alexis Gonzalez <alexis.gonzalez07@gmail.com>
2022-05-30 13:02:52 -0500

    Started learning how to implement O(n) algorithms

---
+++ Activity/Problem Solving Approach/multiplePointers.js
@@ -0,0 +1,10 @@
+// Implement a function called countUniqueValues which accepts a sorted array, and counts the unique values in the array. There can be negative numebrs in the arrya, but it will always be sorted. 
+
+const countUniqueValues=(arr) => {
+    let start = 0
+    let end = arr.length -1
+    let counter = {}
+    while (start <end){
+        
+    }
+}
