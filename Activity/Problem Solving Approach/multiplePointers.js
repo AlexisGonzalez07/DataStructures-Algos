313a9215d7b09641f03b3e3c134fedbc43648675
Alexis Gonzalez <alexis.gonzalez07@gmail.com>
2022-06-10 08:57:08 -0500

    Created count unique values function

--- Activity/Problem Solving Approach/multiplePointers.js
+++ Activity/Problem Solving Approach/multiplePointers.js
@@ -1,10 +1,14 @@
 // Implement a function called countUniqueValues which accepts a sorted array, and counts the unique values in the array. There can be negative numebrs in the arrya, but it will always be sorted. 
 
 const countUniqueValues=(arr) => {
-    let start = 0
-    let end = arr.length -1
     let counter = {}
-    while (start <end){
-        
+    for(val of arr){
+        counter[val] = (counter[val]||0) +1
+        console.log(counter)
     }
+    console.log(Object.keys(counter).length)
+    return Object.keys(counter).length
 }
+
+countUniqueValues([1,2,3,4,4,4,4])
+
